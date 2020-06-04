44f3efb553214213ab80c5abda0080af16f18c7f
sjkang <2fari5@naver.com>
2020-06-05 05:10:57 +0900

    1. 게임 플레이 방식 대폭 변경 2. 플레이어 아이콘 변경

--- Main.java
+++ Main.java
@@ -1,69 +1,111 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-class Intro extends JComponent {
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
-  @Override
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    g.setColor(Color.LIGHT_GRAY);
-    g.drawLine(0,400 , 800, 400);
-    g.drawRect(90, 370, 30, 30);
-    g.fillRect(90,370,30,30);
-    g.setColor(Color.BLACK);
-    g.setFont(g.getFont().deriveFont(g.getFont().getSize() * 2.0F));
-    g.drawString("Press Any Key to Start", 275, 285);
-  }
-}
+public class Main extends JFrame {
+	private Image bufferImage;
+	private Graphics screenGraphic;
 
+	private Clip clip;
 
-public class Main {
+	private Image player = new ImageIcon("src/images/kitty.png").getImage();
 
-  static JFrame frame;
-  static boolean gameplaying = false;
-  Main() {
-    frame = new JFrame();
-    frame.setSize(800,600);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(new Intro());
+	private int playerX, playerY;
+	private int playerWidth = player.getWidth(null);
+	private int playerHeight = player.getHeight(null);
 
-    frame.addKeyListener(new KeyListener(){
+	private boolean up, down, left, right;
 
-      @Override
-      public void keyTyped(KeyEvent arg0) {
-      }
+	public Main() {
+		setTitle("Kitty Game");
+		setVisible(true);
+		setSize(500,500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_W:
+					up = true;
+					break;
+				case KeyEvent.VK_S:
+					down = true;
+					break;
+				case KeyEvent.VK_A:
+					left = true;
+					break;
+				case KeyEvent.VK_D:
+					right = true;
+					break;
+				}
+			}
 
-      @Override
-      public void keyReleased(KeyEvent arg0) {
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_W:
+					up = false;
+					break;
+				case KeyEvent.VK_S:
+					down = false;
+					break;
+				case KeyEvent.VK_A:
+					left = false;
+					break;
+				case KeyEvent.VK_D:
+					right = false;
+					break;
+				}
+			}
+		});
 
-      }
+		while (true) {
+			try {
+				Thread.sleep(30);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			keyProcess();
 
-      @Override
-      public void keyPressed(KeyEvent arg0) {
-        if(!gameplaying && Game.gameOver)
-          System.exit(1);
-        if(!gameplaying) {
-          frame.getContentPane().removeAll();
-            new Game();
-        }
-      }
-    });
+		}
+	}
 
-    frame.setVisible(true);
+	public void keyProcess() {
+		if (up && playerY - 3 > 30)
+			playerY -= 3;
+		if (down && playerY + playerHeight + 3 < 500)
+			playerY += 3;
+		if (left && playerX - 3 > 0)
+			playerX -= 3;
+		if (right && playerX + playerWidth + 3 < 500)
+			playerX += 3;
+	}
 
-  }
+	public void paint(Graphics g) {
+		bufferImage = createImage(500,500);
+		screenGraphic = bufferImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(bufferImage, 0, 0, null);
+	}
 
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Runnable(){
+	public void screenDraw(Graphics g) {
 
-      @Override
-      public void run() {
-        new Main();
-      }
-    });
-  }
+		g.drawImage(player, playerX, playerY, null);
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Arial", Font.BOLD, 40));
 
+		this.repaint();
+	}
 
+	public static void main(String[] args) {
+		new Main();
+	}
 
 }
