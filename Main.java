c239a448e59dcaf0c9334268be299afb8b3c6fc0
youngho <hyhjiyean@naver.com>
2020-06-07 15:09:12 +0900

    플레이어가 획득할 아이템 추가

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Window;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
+import javax.print.attribute.standard.Media;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -20,13 +22,15 @@ public class Main extends JFrame {
 	private Graphics screenGraphic;
 	private Clip clip;
 	private int fishX, fishY;
-	private int score;
-    
+	private int meatX, meatY;
 	
-	private Image player = new ImageIcon("src/images/kitty1.png").getImage();
-    private Image background = new ImageIcon("src/images/h2.png").getImage();
-    private Image fish = new ImageIcon("src/images/fish1.png").getImage();
+	private int score, Time;
 
+	private Image player = new ImageIcon("src/images/kitty1.png").getImage();
+	private Image background = new ImageIcon("src/images/h2.png").getImage();
+	private Image fish = new ImageIcon("src/images/fish1.png").getImage();
+	private Image meat = new ImageIcon("src/images/meat.png").getImage();
+	
 	private int playerX, playerY;
 	private int playerWidth = player.getWidth(null);
 	private int playerHeight = player.getHeight(null);
@@ -35,14 +39,17 @@ public class Main extends JFrame {
 
 	public Main() {
 		setTitle("Kitty Game");
-		playSound("src/sound/BGM.wav", true); 
+		playSound("src/sound/BGM.wav", true);
 		setVisible(true);
-		setSize(1000,900);
+		setSize(1000, 900);
 		fishX = 250;
 		fishY = 250;
+		meatX=300;
+		meatY=300;
 		score = 0;
-		playerX=10;
-		playerY=35;
+		playerX = 10;
+		playerY = 35;
+
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -81,7 +88,7 @@ public class Main extends JFrame {
 				}
 			}
 		});
-		
+
 		while (true) {
 			try {
 				Thread.sleep(20);
@@ -92,6 +99,7 @@ public class Main extends JFrame {
 
 		}
 	}
+
 	public void playSound(String pathName, boolean isLoop) {
 		try {
 			clip = AudioSystem.getClip();
@@ -101,6 +109,7 @@ public class Main extends JFrame {
 			clip.start();
 			if (isLoop)
 				clip.loop(Clip.LOOP_CONTINUOUSLY);
+		
 		} catch (LineUnavailableException e) {
 			e.printStackTrace();
 		} catch (UnsupportedAudioFileException e) {
@@ -108,7 +117,8 @@ public class Main extends JFrame {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}	// 오디오 재생
+		
+	} // 오디오 재생
 
 	public void keyProcess() {
 		if (up && playerY - 3 > 30)
@@ -122,7 +132,7 @@ public class Main extends JFrame {
 	}
 
 	public void paint(Graphics g) {
-		bufferImage = createImage(1000,900);
+		bufferImage = createImage(1000, 900);
 		screenGraphic = bufferImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(bufferImage, 0, 0, null);
@@ -131,18 +141,20 @@ public class Main extends JFrame {
 	public void screenDraw(Graphics g) {
 
 		g.drawImage(background, 0, 0, null);
-		g.drawImage(player,  playerX, playerY, null);
+		g.drawImage(player, playerX, playerY, null);
 		g.setColor(Color.RED);
 		g.setFont(new Font("Arial", Font.BOLD, 35));
 		g.drawString("SCORE : " + score, 750, 62);
 		g.drawImage(fish, fishX, fishY, null);
-	
+		g.drawImage(meat, meatX, meatY, null);
+		g.drawString("Time : " + Time, 510, 60);
 
 		this.repaint();
 	}
 
 	public static void main(String[] args) {
 		new Main();
+	
 	}
 
 }
